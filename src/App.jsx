bfb2452f1005b2aa4541b8cf80f62e66f052ff9f
JH <wlgud5632@naver.com>
2025-02-11 16:36:57 +0900

    네브바 기본틀

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
 import Home from "./pages/Home";
+import Header from "./components/layout/Header";
 
 function App() {
   return (
     <div>
       <BrowserRouter>
+        <Header />
         <Routes>
           <Route path="/" element={<Home />} />
         </Routes>
