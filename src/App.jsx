e68b7dc383489c68119e76934722769dc82a93df
JH <wlgud5632@naver.com>
2025-02-13 11:29:30 +0900

    네브바 수정 및 마이페이지 생성

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,8 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
 import Home from "./pages/Home";
 import Header from "./components/layout/Header";
+import Notices from "./pages/Notices";
+import Mypage from "./pages/Mypage";
 
 function App() {
   return (
@@ -10,6 +12,8 @@ function App() {
         <Header />
         <Routes>
           <Route path="/" element={<Home />} />
+          <Route path="/notice" element={<Notices />} />
+          <Route path="/mypage" element={<Mypage />} />
         </Routes>
       </BrowserRouter>
     </div>
