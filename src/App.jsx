377d7f59564f55adc191f6a66e6fbb7472aa8c5d
JH <wlgud5632@naver.com>
2025-02-13 16:10:02 +0900

    마이페이지 DB연결 , 네브바 오류, 메인사진 수정, 예약 페이지만 생성

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Home from "./pages/Home";
 import Header from "./components/layout/Header";
 import Notices from "./pages/Notices";
 import Mypage from "./pages/Mypage";
+import Reservations from "./pages/Reservations";
 
 function App() {
   return (
@@ -13,7 +14,8 @@ function App() {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/notice" element={<Notices />} />
-          <Route path="/mypage" element={<Mypage />} />
+          <Route path="/mypage/:id" element={<Mypage />} />
+          <Route path="/reservation" element={<Reservations />} />
         </Routes>
       </BrowserRouter>
     </div>
