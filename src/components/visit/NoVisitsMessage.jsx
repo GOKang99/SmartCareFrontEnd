5d2214e58b07b255fc506e4bbf8d736c06a6c5e1
omer <justletomer@gmail.com>
2025-02-18 14:12:20 +0900

    예약 없음 메시지 컴포넌트 추가,

---
+++ src/components/visit/NoVisitsMessage.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const NoVisitsMessage = () => {
+  return (
+    <div className="w-full h-32 flex items-center justify-center text-lg font-semibold text-gray-600">
+      아직 예약이 없습니다. 예약을 생성하세요!
+    </div>
+  );
+};
+
+export default NoVisitsMessage;
