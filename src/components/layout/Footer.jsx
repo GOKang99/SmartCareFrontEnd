6d82f261a7ef5553da43a58dabaf9a0c70ba9e9b
dudrhkdspt112 <dudrhkdspt112@gmail.com>
2025-03-11 13:47:24 +0900

    헤더 메인페이지 디자인완료

--- src/components/layout/Footer.jsx
+++ src/components/layout/Footer.jsx
@@ -1,7 +1,9 @@
+import "./Footer.css"
 const Footer = () => {
   return (
-    <footer className="w-full bg-gray-800 text-white text-center py-3 mt-auto">
+    <footer className="w-full bg-gray-700 text-white text-center py-3 mt-auto">
       © 2025 SmartCare. All Rights Reserved.
+      Created as a team project
     </footer>
   );
 };
--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -3,6 +3,7 @@ import { Link, useLocation, useNavigate } from "react-router-dom";
 import { useMyContext } from "../../ContextApi";
 import { toast } from "react-toastify";
 import { jwtDecode } from "jwt-decode";
+import "./Header.css"
 
 const Header = () => {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
@@ -30,14 +31,14 @@ const Header = () => {
 
   return (
     <>
-      <nav className="bg-white border-gray-200 dark:bg-gray-900">
+      <nav className="bg-white border-gray-200 dark:bg-gray-900 top-0 left-0 w-full z-10  shadow-sm">
         <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-6">
           <Link
             to="/"
             className="flex items-center space-x-3 rtl:space-x-reverse"
           >
             <img src="/icon.png" className="h-8" alt="SmartCare Logo" />
-            <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">
+            <span className="self-center text-2xl font-bold whitespace-nowrap dark:text-white">
               SmartCare
             </span>
           </Link>
@@ -75,9 +76,9 @@ const Header = () => {
             } w-full md:block md:w-auto`}
             id="navbar-default"
           >
-            <ul className="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
+            <ul className="menu font-bold flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
               <li>
-                <Link
+                {/* <Link
                   to="/"
                   className={`block py-2 px-3 rounded-sm md:p-0 ${
                     location.pathname === "/"
@@ -86,6 +87,18 @@ const Header = () => {
                   }`}
                 >
                   Home
+                </Link> */}
+              </li>
+              <li className={location.pathname === "/notice" ? "active" : ""}>
+                <Link
+                  to="/notice"
+                  className={`block py-2 px-3 rounded-sm md:p-0 ${
+                    location.pathname === "/notice"
+                      ? "text-blue-700"
+                      : "text-gray-800 hover:text-blue-700"
+                  }`}
+                >
+                  공지사항
                 </Link>
               </li>
               <li>
@@ -94,22 +107,10 @@ const Header = () => {
                   className={`block py-2 px-3 rounded-sm md:p-0 ${
                     location.pathname === "/givers"
                       ? "text-blue-700"
-                      : "text-gray-900 hover:text-blue-700"
+                      : "text-gray-800 hover:text-blue-700"
                   }`}
                 >
                   요양사 소개
-                </Link>
-              </li>
-              <li>
-                <Link
-                  to="/notice"
-                  className={`block py-2 px-3 rounded-sm md:p-0 ${
-                    location.pathname === "/notice"
-                      ? "text-blue-700"
-                      : "text-gray-900 hover:text-blue-700"
-                  }`}
-                >
-                  공지사항
                 </Link>
               </li>
               <li>
@@ -118,13 +119,13 @@ const Header = () => {
                   className={`block py-2 px-3 rounded-sm md:p-0 ${
                     location.pathname === "/resident"
                       ? "text-blue-700"
-                      : "text-gray-900 hover:text-blue-700"
+                      : "text-gray-800 hover:text-blue-700"
                   }`}
                 >
                   입소자 정보
                 </Link>
               </li>
-              <li>
+              {/* <li>
                 <Link
                   to="/composition"
                   className={`block py-2 px-3 rounded-sm md:p-0 ${
@@ -135,14 +136,14 @@ const Header = () => {
                 >
                   체성분 분석
                 </Link>
-              </li>
+              </li> */}
               <li>
                 <Link
                   to="/visits"
                   className={`block py-2 px-3 rounded-sm md:p-0 ${
                     location.pathname === "/visits"
                       ? "text-blue-700"
-                      : "text-gray-900 hover:text-blue-700"
+                      : "text-gray-800 hover:text-blue-700"
                   }`}
                 >
                   방문예약
@@ -154,7 +155,7 @@ const Header = () => {
                   className={`block py-2 px-3 rounded-sm md:p-0 ${
                     location.pathname === "/status"
                       ? "text-blue-700"
-                      : "text-gray-900 hover:text-blue-700"
+                      : "text-gray-800 hover:text-blue-700"
                   }`}
                 >
                   생활현황
@@ -166,7 +167,7 @@ const Header = () => {
                   className={`block py-2 px-3 rounded-sm md:p-0 ${
                     location.pathname === "/mypage"
                       ? "text-blue-700"
-                      : "text-gray-900 hover:text-blue-700"
+                      : "text-gray-800 hover:text-blue-700"
                   }`}
                 >
                   마이페이지
@@ -179,7 +180,7 @@ const Header = () => {
                     className={`block py-2 px-3 rounded-sm md:p-0 ${
                       location.pathname === "/signup"
                         ? "text-blue-700"
-                        : "text-gray-900 hover:text-blue-700"
+                        : "text-gray-800 hover:text-blue-700"
                     }`}
                   >
                     회원가입
@@ -187,9 +188,9 @@ const Header = () => {
                 </li>
               )}
               {token ? (
-                <li onClick={handleLogout} className="cursor-pointer">
-                  로그아웃
-                </li>
+                  <li onClick={handleLogout} className="cursor-pointer text-gray-800">
+                    로그아웃
+                  </li>
               ) : (
                 <li>
                   <Link
@@ -197,7 +198,7 @@ const Header = () => {
                     className={`block py-2 px-3 rounded-sm md:p-0 ${
                       location.pathname === "/login"
                         ? "text-blue-700"
-                        : "text-gray-900 hover:text-blue-700"
+                        : "text-gray-800 hover:text-blue-700"
                     }`}
                   >
                     로그인
