63c4a2d1925c279e6ddb3c9c9371da5f961c37ad
omer <justletomer@gmail.com>
2025-03-12 11:37:50 +0900

    스피너 visit에 등록

--- src/components/visit/VisitLisitForGuard.jsx
+++ src/components/visit/VisitLisitForGuard.jsx
@@ -4,10 +4,12 @@ import ErrorMessage from "../form/ErrorMessage";
 import VisitItem from "./VisitItem";
 import NoVisitsMessage from "./NoVisitsMessage";
 import { useMyContext } from "../../ContextApi";
+import Spinner from "../../utils/Spinner";
 
 const VisitLisitForGuard = () => {
   const [visits, setVisits] = useState([]);
   const [error, setError] = useState("");
+  const [isLoading, setLoading] = useState(false);
 
   //guardId ContextPath에서 받아오기
   const { guardId } = useMyContext();
@@ -16,20 +18,24 @@ const VisitLisitForGuard = () => {
 
   // 보호자 예약 데이터 불러오기
   useEffect(() => {
-    const fetchAllVisits = async () => {
-      try {
-        const response = await api.get(`/visit/all/${guardId}`);
-        setVisits(response.data);
-      } catch (error) {
-        console.error(error);
-        setError(guardId + "의 예약 데이터 불러오기 실패");
-      }
-    };
-    const timeoutId = setTimeout(() => {
-      fetchAllVisits();
-    }, 100); // 1초 (1000ms)
+    setLoading(true);
+    if (guardId) {
+      const fetchAllVisits = async () => {
+        try {
+          const response = await api.get(`/visit/all/${guardId}`);
+          setVisits(response.data);
+        } catch (error) {
+          console.error(error);
+          setError(guardId + "의 예약 데이터 불러오기 실패");
+        }
+        setLoading(false);
+      };
+      const timeoutId = setTimeout(() => {
+        fetchAllVisits();
+      }, 100); // 1초 (1000ms)
 
-    return () => clearTimeout(timeoutId); // 컴포넌트 언마운트 시 타이머 정리
+      return () => clearTimeout(timeoutId); // 컴포넌트 언마운트 시 타이머 정리
+    }
   }, [guardId]); // guardId가 변경될 때마다 실행되도록 의존성 배열에 추가
 
   const handleVisitUpdate = (updatedVisit) => {
@@ -41,6 +47,14 @@ const VisitLisitForGuard = () => {
       )
     );
   };
+
+  if (isLoading) {
+    return (
+      <div className="w-full h-screen flex justify-center items-center">
+        <Spinner />
+      </div>
+    );
+  }
 
   return (
     <div className="p-4 space-y-4 w-[1000px] mx-auto ">
